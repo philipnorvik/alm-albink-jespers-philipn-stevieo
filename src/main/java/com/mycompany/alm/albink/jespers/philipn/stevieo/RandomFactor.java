35b767ece948d39f9f61017891500b9458855215
albinkj91 <albinkj91@yahoo.com>
2019-09-03 13:40:45 +0200

    Added classes

---
+++ src/main/java/com/mycompany/alm/albink/jespers/philipn/stevieo/RandomFactor.java
@@ -0,0 +1,10 @@
+package com.mycompany.alm.albink.jespers.philipn.stevieo;
+
+public class RandomFactor {
+    
+    private RandomFactor(){}
+    
+    public static int zeroToNine(){
+        return (int) (Math.random() * 9);
+    }
+}
