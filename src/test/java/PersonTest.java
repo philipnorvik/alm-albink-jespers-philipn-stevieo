9a4448b1b2529c5b0cc72f0f5a733ee9aff59a5e
SjostromJesper <j.sjostrom@live.se>
2019-09-05 10:47:32 +0200

    new tests

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,28 @@
+import com.mycompany.alm.albink.jespers.philipn.stevieo.Person;
+import com.mycompany.alm.albink.jespers.philipn.stevieo.RandomFactor;
+import com.mycompany.alm.albink.jespers.philipn.stevieo.Translator;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PersonTest {
+    Person p = new Person("knut", 58, "male");
+    
+    @Test
+    public void nameTest() {
+        String expected = "knut";
+        
+        assertTrue(expected.equalsIgnoreCase(p.getName()));
+    }
+    
+    public void ageTest() {
+        int expected = 58;
+        
+        assertTrue(expected == p.getAge());
+    }
+    
+    public void genderTest() {
+        String expected = "male";
+        
+        assertTrue(expected.equalsIgnoreCase(p.getGender()));
+    }
+}
